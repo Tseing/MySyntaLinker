2b77ec739aefc484327a989f0bdfe6ca0b9f76f8
Tseing <im.yczeng@foxmail.com>
2023-02-11 16:10:39 +0800

    optimize plot.py

--- constrain_plot.py
+++ constrain_plot.py
@@ -1,16 +1,30 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-x = ["Equal linker length", "Linker length difference ≤ 1"]
-y = [93.7, 97.0]
+def mark_num_top(x, y, bias=2):
+    for i, value in enumerate(y):
+        plt.text(x[i], value + bias, value, ha='center', fontsize=13)
+
+
+labels = ["Equal linker length", "Linker length difference ≤ 1"]
+x = np.arange(len(labels))
+syntalinker = [67.4, 82.4]
+syntalinker_n = [38.3, 62.4]
+
+bar_width = 0.3
+n = 2
 
 plt.ylim(0, 120)
 plt.xticks(fontsize=13)
 plt.ylabel("Percentage (%)", fontsize=13)
-plt.bar(x, y, width=0.3, label="SyntaLinker", color="#2b3d6b")
+plt.bar(x, syntalinker, width=bar_width, label="SyntaLinker", color="#2b3d6b")
+plt.bar(x + bar_width, syntalinker_n, width=bar_width, label="SyntaLinker", color="#e27f82")
+plt.xticks(x + bar_width * (n - 1) / 2, labels)
 
-for x, value in enumerate(y):
-    plt.text(x, value+2, value, ha='center', fontsize=13)
+mark_num_top(x, syntalinker)
+mark_num_top(x + bar_width, syntalinker_n)
+
 plt.legend(loc='upper right', fontsize=13, frameon=False)
 plt.show()
 # plt.savefig("constrain.svg")
--- property_plot.py
+++ property_plot.py
@@ -1,24 +1,30 @@
 import pickle
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 from scipy.stats import gaussian_kde
 
-# output = pickle.load(open("out_property_1.pkl", 'rb'))
-output_n = pickle.load(open("property_SyntaLinker_top10.pkl", 'rb'))
+output = pickle.load(open("property_SyntaLinker_top10.pkl", 'rb'))
+output_n = pickle.load(open("property_SyntaLinker_n_top10.pkl", 'rb'))
 target = pickle.load(open("property_ChEMBL.pkl", 'rb'))
 
-# for key in output:
-#     output[key] = list(filter(None, output[key]))
 
-for key in output_n:
-    output_n[key] = list(filter(None, output_n[key]))
+def is_value(item):
+    if item == "" or item is None or np.isnan(item):
+        return False
+    else:
+        return True
 
-# output_f = {key: None for key in output}
-# for key in output_f:
-#     output_f[key] = gaussian_kde(output[key])
 
-properties = [key for key in output_n]
+def get_gaussian_ked(data_dict: dict):
+    f = {key: None for key in data_dict}
+    for key in properties:
+        data_dict[key] = np.array(list(filter(is_value, data_dict[key])))
+        f[key] = gaussian_kde(data_dict[key])
+
+    return f
+
+
+properties = [key for key in target]
 index = 0
 row, col = 2, 2
 fig, subs = plt.subplots(row, col)
@@ -27,21 +33,21 @@ plt.subplots_adjust(wspace=0.55, hspace=0.3)
 for i in range(row):
     for j in range(col):
         property = properties[index]
-        # _, out_bins, _ = subs[i][j].hist(output[property], bins=200,
-        #                                  density=True, alpha=0.3, color="C2")
+        _, out_bins, _ = subs[i][j].hist(output[property], bins=200,
+                                         density=True, alpha=0.3, color="C2")
         _, out_n_bins, _ = subs[i][j].hist(output_n[property], bins=200,
-                                           density=True, alpha=0.3, color="C2")
+                                           density=True, alpha=0.3, color="C1")
         _, tgt_bins, _ = subs[i][j].hist(target[property], bins=200,
                                          density=True, alpha=0.3, color="C0")
 
-        # subs[i][j].plot(out_bins[:-1], output_f[property](out_bins[:-1]),
-        #                 color="C2", label="ChEMBL Set")
-        # subs[i][j].plot(out_n_bins[:-1], output_f[property](out_n_bins[:-1]),
-        #                 color="C1", label="SyntaLinker_n")
-        # subs[i][j].plot(tgt_bins[:-1], output_f[property](tgt_bins[:-1]),
-        #                 color="C0", label="SyntaLinker")
+        subs[i][j].plot(out_bins[:-1], get_gaussian_ked(target)[property](out_bins[:-1]),
+                        color="C2", label="ChEMBL Set")
+        subs[i][j].plot(out_n_bins[:-1], get_gaussian_ked(output_n)[property](out_n_bins[:-1]),
+                        color="C1", label="SyntaLinker_n")
+        subs[i][j].plot(tgt_bins[:-1], get_gaussian_ked(output)[property](tgt_bins[:-1]),
+                        color="C0", label="SyntaLinker")
         subs[i][j].set_xlabel(property, fontsize=11)
-        # subs[i][j].legend(fontsize=6, frameon=False)
+        subs[i][j].legend(fontsize=6, frameon=False)
 
         index += 1
 
